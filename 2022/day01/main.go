5d661d850e5110937ade0d906de00a049f5e7cf3
andrealombardi <andrea.lombardi@gmail.com>
2022-12-01 23:49:44 +0000

    clean up day01

--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -10,44 +10,50 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	fmt.Println(calculate(newMaxComparator()))
+}
+
+func part2() {
+	fmt.Println(calculate(newTop3Comparator()))
+}
+
+type comparator func(int) int
+
+func newMaxComparator() comparator {
+	max := 0
+	return func(i int) int {
+		if i > max {
+			max = i
+		}
+		return max
+	}
+}
+
+func newTop3Comparator() comparator {
+	top3 := []int{0, 0, 0}
+	return func(i int) int {
+		if i > top3[0] {
+			top3[0] = i
+			sort.Ints(top3)
+		}
+		return top3[0] + top3[1] + top3[2]
+	}
+}
+
+func calculate(c comparator) int {
+
 	content, err := os.ReadFile("day01/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	part1(string(content))
-	part2(string(content))
-}
-
-func part1(content string) {
-
-	groups := strings.Split(content, "\n\n")
-	max := 0
-
-	for _, group := range groups {
-		calories := strings.Split(group, "\n")
-		sum := 0
-		for _, calory := range calories {
-			if calory == "" {
-				continue
-			}
-			cal, err := strconv.Atoi(calory)
-			if err != nil {
-				log.Fatal(err)
-			}
-			sum += cal
-		}
-		if sum > max {
-			max = sum
-		}
-
-	}
-	fmt.Println(max)
-}
-
-func part2(content string) {
 
 	groups := strings.Split(string(content), "\n\n")
-	top3 := []int{0, 0, 0}
+	total := 0
 
 	for _, group := range groups {
 		calories := strings.Split(group, "\n")
@@ -62,13 +68,63 @@ func part2(content string) {
 			}
 			sum += cal
 		}
-
-		if sum > top3[0] {
-			top3[0] = sum
-			sort.Ints(top3)
-		}
-
+		total = c(sum)
 	}
-
-	fmt.Println(top3[0] + top3[1] + top3[2])
+	return total
 }
+
+//
+//func part1(content string) {
+//
+//	groups := strings.Split(content, "\n\n")
+//	max := 0
+//
+//	for _, group := range groups {
+//		calories := strings.Split(group, "\n")
+//		sum := 0
+//		for _, calory := range calories {
+//			if calory == "" {
+//				continue
+//			}
+//			cal, err := strconv.Atoi(calory)
+//			if err != nil {
+//				log.Fatal(err)
+//			}
+//			sum += cal
+//		}
+//		if sum > max {
+//			max = sum
+//		}
+//
+//	}
+//	fmt.Println(max)
+//}
+//
+//func part2(content string) {
+//
+//	groups := strings.Split(string(content), "\n\n")
+//	top3 := []int{0, 0, 0}
+//
+//	for _, group := range groups {
+//		calories := strings.Split(group, "\n")
+//		sum := 0
+//		for _, calory := range calories {
+//			if calory == "" {
+//				continue
+//			}
+//			cal, err := strconv.Atoi(calory)
+//			if err != nil {
+//				log.Fatal(err)
+//			}
+//			sum += cal
+//		}
+//
+//		if sum > top3[0] {
+//			top3[0] = sum
+//			sort.Ints(top3)
+//		}
+//
+//	}
+//
+//	fmt.Println(top3[0] + top3[1] + top3[2])
+//}
